a7f3e5f8a69dfea8929b140a28cf0167a8510a17
Janet Warren <jewljbl13@aol.com>
2018-05-11 11:27:35 -0400

    framework for group project

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var bodyParser=require('body-parser');
+var exphbs = require('express-handlebars');
+var routes=require('./controllers/monarch_controller.js')
+var db = require('./models');
+var PORT = process.env.PORT || 8080;
+var app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.engine('handlebars',exphbs({defaultLayout:'main'}));
+app.set('view engine', 'handlebars');
+
+app.use(routes);
+db.sequelize.sync({force:true}).then(function(){
+    app.listen(PORT, function(){
+        console.log("server listening on: https://localhost:"+PORT);
+    });
+});
