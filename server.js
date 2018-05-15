932950f16dcaaaa3536b1a1cb31340953c70ba1c
Unknown <charlesjgilker@gmail.com>
2018-05-15 18:19:48 -0400

    messing with handlebars

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ require("./routes/subject-api-routes.js")(app);
 require("./routes/comments-api-routes.js")(app);
 //app.use(routes);
 
-db.sequelize.sync().then(function(){
+db.sequelize.sync({force: true}).then(function(){
     app.listen(PORT, function(){
         console.log("server listening on: https://localhost:"+PORT);
     });
