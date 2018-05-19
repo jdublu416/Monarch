206c743577391f94ce9697ad72c53b103681a8ad
Jdublu <jewljbl13@aol.com>
2018-05-19 12:01:31 -0400

    new routes/corrections for post

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ require("./routes/comments-api-routes.js")(app);
 // require("./routes/html-routes.js")(app);
 // app.use('./routes');
 
-db.sequelize.sync({force: false}).then(function(){
+
 
 db.sequelize.sync({force: false}).then(function(){
 
