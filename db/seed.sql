cb273bc251236fb9549575cd1147a6769a054139
Jdublu <jewljbl13@aol.com>
2018-05-20 19:38:37 -0400

    update the config file with heroku info

--- db/seed.sql
+++ db/seed.sql
@@ -6,8 +6,7 @@ INSERT INTO
         auth_email,
         auth_password,
         createdAt,
-        updatedAt,
-        auth_id
+        updatedAt
     )
 VALUES
     (
@@ -17,49 +16,43 @@ VALUES
         "nope@nope.nope",
         "p4ssw0rd",
         CURRENT_TIMESTAMP(),
-        CURRENT_TIMESTAMP(),
-        1
+        CURRENT_TIMESTAMP()
     );
 INSERT INTO
     subjects (
         subj_title,
         subj_desc,
         createdAt,
-        updatedAt,
-        subj_id
+        updatedAt
     )
 VALUES
     (
         "Vidya Games",
         "Pew Pew",
         CURRENT_TIMESTAMP(),
-        CURRENT_TIMESTAMP(),
-        1
+        CURRENT_TIMESTAMP()
     ),
     (
         "Music",
         "Wub Wub",
         CURRENT_TIMESTAMP(),
-        CURRENT_TIMESTAMP(),
-        2
+        CURRENT_TIMESTAMP()
     ),
     (
         "Cooking",
         "Om Nom",
         CURRENT_TIMESTAMP(),
-        CURRENT_TIMESTAMP(),
-        3
+        CURRENT_TIMESTAMP()
     );
 INSERT INTO
     posts (
         post_title,
         post_body,
         post_vote,
-        subj_id,
-        auth_id,
         createdAt,
         updatedAt,
-        post_id
+        authorId,
+        subjectid
     )
 VALUES
     (
@@ -69,6 +62,5 @@ VALUES
         1,
         1,
         CURRENT_TIMESTAMP(),
-        CURRENT_TIMESTAMP(),
-        1
+        CURRENT_TIMESTAMP()
     );
---
+++ test/test.js
@@ -0,0 +1,28 @@
+// var expect = require("chai").expect; 
+
+//THis is an example of possible test format for posting to database for example: 
+// describe("POST", function() {
+ //  it("should return '1 row changed' when create post is clicked", function(){
+//  expect(return msg 200)
+ //});
+//}); you get the picture
+
+
+// var multiply = function(x, y) {
+//   if (typeof x !== "number" || typeof y !== "number") {
+//     throw new Error("x or y is not a number.");
+//   }
+//   else return x * y;
+// };
+
+// describe("Multiply", function() {
+//   it("should multiply properly when passed numbers", function() {
+//     expect(multiply(2, 4)).to.equal(8);
+//   });
+
+//   it("should throw when not passed numbers", function() {
+//     expect(function() {
+//       multiply(2, "4");
+//     }).to.throw(Error);
+//   });
+// });
